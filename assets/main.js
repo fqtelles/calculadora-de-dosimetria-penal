35ee38bd484f5bf726d019ca39456386127011b2
Felipe <telles7@gmail.com>
2023-07-20 09:24:43 -0300

    aperfeiçoadas algumas hipóteses de cálculo

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,7 @@ function calcularResultado() {
   
     const operacao = document.querySelector('input[name="operacao"]:checked').value;
 
-
+// implementar aqui como lidar com valor de meses > 12 e dias > 30
   
     if (operacao === "soma") {
       const anosMeio = anosInicial / denominador * numerador;
@@ -46,10 +46,47 @@ function calcularResultado() {
     var mesesFinal = mesesTotal + mesesExtra;
     var diasFinal = diasTotal + diasExtra;
 
+    // Converter resultados maiores que 12 meses em ano
+    if (mesesFinal > 11) {
+      anosFinal++;
+      var mesesFinalFinal = mesesFinal % 12;
+    }
+
+    // Converter resultados maiores que 30 dias em mês
+    if (diasFinal > 29) {
+      mesesFinal++;
+      var diasFinalFinal = diasFinal % 30;
+    }
+
+    // Converter mes > 11 && dia > 29
+    // if (diasFinal > 29 && mesesFinal > 11) {
+    //   mesesFinal++;
+    //   anosFinal++;
+    //   var diasFinalFinal = diasFinal % 30;
+    //   var mesesFinalFinal = mesesFinal % 12;
+    // }
+
+
     // Exibir o resultado detalhado
     let resultadoDetalhado = "";
-    resultadoDetalhado += Math.floor(anosFinal) + " ano(s), " + Math.floor(mesesFinal) + " mês(es), " + Math.floor(diasFinal) + " dia(s)";
-  
+
+    // o Resultado depende do uso da variável FinalFinal ou não
+    if (typeof mesesFinalFinal === 'undefined' && diasFinalFinal === 'undefined') {
+      resultadoDetalhado += Math.floor(anosFinal) + " ano(s), " + Math.floor(mesesFinal) + " mês(es), " + Math.floor(diasFinal) + " dia(s)";
+    }
+    
+    else if (typeof mesesFinalFinal === 'undefined'){
+      resultadoDetalhado += Math.floor(anosFinal) + " ano(s), " + Math.floor(mesesFinal) + " mês(es), " + Math.floor(diasFinalFinal) + " dia(s)";
+    }
+
+    else if (typeof diasFinalFinal === 'undefined'){
+      resultadoDetalhado += Math.floor(anosFinal) + " ano(s), " + Math.floor(mesesFinalFinal) + " mês(es), " + Math.floor(diasFinal) + " dia(s)";
+    }
+
+    else if (typeof mesesFinalFinal !== 'undefined' && diasFinalFinal !== 'undefined') {
+      resultadoDetalhado += Math.floor(anosFinal) + " ano(s), " + Math.floor(mesesFinalFinal) + " mês(es), " + Math.floor(diasFinalFinal) + " dia(s)";
+    }
+
     document.getElementById("resultado").textContent = resultadoDetalhado;
   
     // Impedir o envio do formulário
